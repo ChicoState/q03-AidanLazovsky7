48d8ef6c1935f112fae33d08eac9f8c57c26d1b7
AidanLazovsky7 <awlazovsky@mail.csuchico.edu>
2025-04-30 20:52:12 -0700

    Created tests for guesser.cpp

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -15,8 +15,134 @@ class GuesserTest : public ::testing::Test
 };
 
 // Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
+TEST(GuesserTest, match_first)
 {
   Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+  ASSERT_TRUE(object.match("Secret"));
 }
+
+TEST(GuesserTest, fail_off_end)
+{
+  Guesser object("Secret");
+  ASSERT_FALSE(object.match("Secre"));
+}
+
+TEST(GuesserTest, fail_off_start)
+{
+  Guesser object("Secret");
+  ASSERT_FALSE(object.match("ecret"));
+}
+
+TEST(GuesserTest, match_second_close)
+{
+  Guesser object("Secret");
+  object.match("secret");
+  ASSERT_TRUE(object.match("Secret"));
+}
+
+TEST(GuesserTest, match_third_close)
+{
+  Guesser object("Secret");
+object.match("secret");
+object.match("secret");
+ASSERT_TRUE(object.match("Secret"));
+}
+
+TEST(GuesserTest, fail_fourth_close)
+{
+  Guesser object("Secret");
+	object.match("secret");
+	object.match("secret");
+	object.match("secret");
+  ASSERT_FALSE(object.match("Secret"));
+}
+
+TEST(GuesserTest, fail_second_far)
+{
+  Guesser object("Secret");
+  object.match("Secretsarenotverycooltohaveunlesstheyarewithmanypeoplethenitiskindacool");
+  ASSERT_FALSE(object.match("Secret"));
+}
+
+TEST(GuesserTest, match_second_edgecase)
+{
+  Guesser object("Secret");
+  object.match("Secret12");
+  ASSERT_TRUE(object.match("Secret"));
+}
+
+TEST(GuesserTest, fail_third_edgecase)
+{
+  Guesser object("Secret");
+  object.match("Secret123");
+  ASSERT_FALSE(object.match("Secret"));
+}
+
+TEST(GuesserTest, match_empty)
+{
+  Guesser object("");
+  ASSERT_TRUE(object.match(""));
+}
+
+TEST(GuesserTest, match_third_and_sixth_close)
+{
+  Guesser object("Secret");
+	object.match("secret");
+	object.match("secret");
+	object.match("Secret");
+	object.match("secret");
+	object.match("secret");
+  ASSERT_TRUE(object.match("Secret"));
+}
+
+TEST(GuesserTest, test_remaining_after_one)
+{
+  Guesser object("Secret");
+	object.match("secret");
+  ASSERT_EQ(2,object.remaining());
+}
+
+TEST(GuesserTest, test_remaining_after_two)
+{
+  Guesser object("Secret");
+	object.match("secret");
+  object.match("secret");
+  ASSERT_EQ(1,object.remaining());
+}
+
+TEST(GuesserTest, test_remaining_after_three)
+{
+  Guesser object("Secret");
+	object.match("secret");
+  object.match("secret");
+    object.match("secret");
+  ASSERT_EQ(0,object.remaining());
+}
+
+TEST(GuesserTest, remaining_reset_first)
+{
+  Guesser object("Secret");
+	object.match("Secret");
+  ASSERT_EQ(3,object.remaining());
+}
+
+TEST(GuesserTest, remaining_reset_second)
+{
+  Guesser object("Secret");
+	object.match("secret");
+	object.match("Secret");
+  ASSERT_EQ(3,object.remaining());
+}
+
+TEST(GuesserTest, remaining_reset_third)
+{
+  Guesser object("Secret");
+	object.match("secret");
+	object.match("secret");
+	object.match("Secret");
+  ASSERT_EQ(3,object.remaining());
+}
+
+
+
+
